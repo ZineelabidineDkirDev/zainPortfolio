588c817dd4c835cf386ff589bdee493214580cb6
Zine EL Abidine DKIR Elmoubtakir <95342581+ZineelabidineDkirDev@users.noreply.github.com>
2025-08-07 12:30:22 +0100

    correct nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -15,7 +15,7 @@ const Nav = () => {
   const pathname = usePathname();
   
   return (
-    <nav className="flex gap-8 fixed top-0 left-0 right-0 z-50">
+    <nav className="flex gap-8">
       {links.map((link, index) => (
         <Link 
           key={index}
