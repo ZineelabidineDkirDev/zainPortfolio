767ff1b6b1c9f3de91300f2933b709dced30a0ea
Zine EL Abidine DKIR Elmoubtakir <95342581+ZineelabidineDkirDev@users.noreply.github.com>
2025-08-09 18:24:09 +0100

    nv 2 Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
   return (
     <header
       className={`fixed top-0 left-0 xl:w-full max-sm:hidden xl:flex items-center gap-8 px-6 xl:py-5 z-50 transition-colors duration-300 ${
-        scrolled ? 'bg-accent text-black shadow-md' : 'bg-transparent text-white'
+        scrolled ? 'bg-accent text-white shadow-md' : 'bg-transparent text-white'
       }`}
     >
       <div className="container xl:mx-auto ml-0 flex justify-between items-center mx-40">
