f702e4cd96fe0a01cfd18eb5fa5730b59bee0eb8
Zine EL Abidine DKIR Elmoubtakir <95342581+ZineelabidineDkirDev@users.noreply.github.com>
2025-08-11 12:38:57 +0100

    model header

--- components/Header.jsx
+++ components/Header.jsx
@@ -23,7 +23,7 @@ const Header = () => {
 
   return (
     <header
-      className={`fixed top-0 left-0 xl:w-full max-sm:hidden xl:flex items-center gap-8 px-6 xl:py-5 z-50 transition-colors duration-300 ${
+      className={`fixed top-0 left-0 xl:w-full max-sm:w-full max-sm:py-11 xl:flex items-center gap-8 px-6 xl:py-5 z-50 transition-colors duration-300 ${
         scrolled ? 'bg-[#352093] text-[#e7e2ff] xl:py-1 transition-all duration-300 shadow-md' : 'bg-transparent text-white'
       }`}
     >
